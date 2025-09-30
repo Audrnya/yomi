3d0547e0cc61714364d454632a749fedb9d192a6
Audrnya <audrnya@gmail.com>
2025-09-30 14:56:08 +0800

    Change jisho-button into a link

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var mirror = document.getElementById("mirror")
-var reflection = document.getElementById("reflection")
+// Shortcut for this long ass function name
+function $(id) {
+	return document.getElementById(id)
+}
+
+
+// HTML Element References 
+var mirror = $("mirror")
+var reflection = $("reflection")
+var jishoAnchor = $("jisho-anchor")
+
+// States
 var mirrorFocus = false
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -9,6 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function _onMirrorInput(value) {
 	reflection.innerText = value
+
+	let userInput = encodeURIComponent(mirror.value)
+	let newUrl = `https://jisho.org/search/${userInput}`
+	jishoAnchor.href = newUrl
 }
 
 function _onReflectionClick() {
@@ -23,11 +37,5 @@ function _onReflectionClick() {
 function _onClearClick() {
 	mirror.value = ""
 	reflection.innerText = ""
-}
-
-function _onJishoClick() {
-	let userInput = encodeURIComponent(mirror.value)
-
-	let newUrl = `https://jisho.org/search/${userInput}`
-	window.location.assign(newUrl)
+	jishoAnchor.href = "https://jisho.org/search/"
 }
